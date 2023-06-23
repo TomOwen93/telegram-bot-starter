6b8d8e0f506d54e888fbb7fa88d6689948e24165
TomOwen93 <mr.tsowen@gmail.com>
2023-06-23 14:07:22 +0100

    Started quotes functionality (hot fuzz)

---
+++ src/data.cjs
@@ -0,0 +1,73 @@
+const quotes = [
+  {
+    character: "Nicholas Angel",
+    quote:
+      "You wanna be a big cop in a small town? Fuck off up the model village.",
+  },
+  {
+    character: "Danny Butterman",
+    quote: "Have you ever fired two guns whilst jumping through the air?",
+  },
+  {
+    character: "Nicholas Angel",
+    quote:
+      "I may not be a man of God, Reverend, but I know right and I know wrong, and I have the good grace to know which is which.",
+  },
+  {
+    character: "Danny Butterman",
+    quote: "What do you think this is, some kind of fucking joke?",
+  },
+  {
+    character: "Nicholas Angel",
+    quote: "The greater good.",
+  },
+  {
+    character: "Danny Butterman",
+    quote: "You're a doctor. Deal with it.",
+  },
+  {
+    character: "Nicholas Angel",
+    quote: "By the power of Greyskull!",
+  },
+  {
+    character: "Danny Butterman",
+    quote:
+      "Is it true that there is a place in a man's head that if you shoot it, it will blow up?",
+  },
+  {
+    character: "Nicholas Angel",
+    quote: "Get out of the car... or I will shoot you in the face.",
+  },
+  {
+    character: "Danny Butterman",
+    quote:
+      "The swan. They're attracted to it. They're like... swan... magnets.",
+  },
+  {
+    character: "Simon Skinner",
+    quote: "People like nostalgia. They like to think of a simpler time.",
+  },
+  {
+    character: "Tom Weaver",
+    quote: "Get the fuck out of the way, Nicholas!",
+  },
+  {
+    character: "Sergeant Tony Fisher",
+    quote: "I'm not made of eyes!",
+  },
+  {
+    character: "Joyce Cooper",
+    quote: "I never heard anyone say so many wrong things one after the other.",
+  },
+  {
+    character: "Frank Butterman",
+    quote: "For the greater good.",
+  },
+  {
+    character: "Andy Wainwright",
+    quote:
+      "The thing is, Andy, it's not important to be right. It's only important to look right.",
+  },
+];
+
+module.exports = quotes;
--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,42 @@
 import { Context, Telegraf } from "telegraf";
 import getBotTokenOrQuit from "./util/getBotToken";
 import { DiceRoll } from "@dice-roller/rpg-dice-roller";
+import { keyboard } from "telegraf/typings/markup";
+import Telegram from "telegraf/typings/telegram";
 
+const quotes = require("./quotes.json");
 const botToken = getBotTokenOrQuit();
 const bot = new Telegraf(botToken);
 
-bot.start((ctx) => ctx.reply("Hello!  Let's talk!"));
+const characters = ["Nicholas Angel", "Danny Butterman"];
+
+let characterContext = "";
+
+bot.command("quotes", (ctx) => {
+  bot.telegram.sendMessage(ctx.chat.id, "Hello!", {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "Nicholas Angel", callback_data: "Nicholas Angel" }],
+      ],
+    },
+  });
+});
+
 bot.help((ctx) => ctx.reply("Hmm i am not programmed to be helpful, yet!"));
-
-// bot.command("dicey", handleDiceCommand);
-
-// function handleDiceCommand(ctx: Context) {
-//   return ctx.reply(new DiceRoll("4d8 + 1d6").total.toString());
-// }
-
 bot.command("dice2", (ctx) => {
   let msg = ctx.message.text;
   const [_junk, ...args] = msg.split(" ");
   return ctx.reply(new DiceRoll(args.join(" ")).total.toString());
 });
 
-bot.hears("hello", (ctx) => ctx.reply("Ok, I heard you say hello"));
 bot.hears("cheese", (ctx) =>
   ctx.replyWithPhoto(
     "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Emmentaler_Premier_Cru.jpg/1200px-Emmentaler_Premier_Cru.jpg"
   )
 );
 
-bot.hears("who", (ctx) =>
-  ctx.reply(pick(["Dave", "Alice", "Bob", "Charlie"]).toUpperCase())
-);
-
-bot.command("sing", (ctx) => ctx.reply("La la la!  I got your command."));
-
 bot.launch();
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
-
-//write function pick() given array[] returns one element of it
-
-function pick<T>(array: T[]): T {
-  let index = Math.floor(Math.random() * array.length);
-  return array[index];
-}
